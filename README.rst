a99f5290b62cbcc38d7fc9ac3a38a5378d808e27
PSJay <psjay.peng@gmail.com>
2014-04-20 21:56:19 +0800

    setup.py

---
+++ README.rst
@@ -0,0 +1,34 @@
+PyLocache
+===============
+
+.. image:: https://travis-ci.org/psjay/PyLocache.svg?branch=master   :target: https://travis-ci.org/psjay/PyLocache
+
+PyLocache is a Python implementation of LRU local cache.
+
+Installation
+===============
+
+::
+
+  $ pip install pylocache
+
+[virtualenv](https://pypi.python.org/pypi/virtualenv) is strongly recommended.
+
+Usage
+===============
+
+::
+
+  from pylocache import LocalCache
+
+
+  cache = LocalCache(max_size=5)
+  cache.set('foo', 1)
+  cache.set('bar', 2)
+
+  cache.get('foo')  # 1
+
+  cache.set('hello', 'world', expires=3)  # expires in 3 seconds.
+
+  # All items of it will be expired in 2 seconds after being set.
+  volatile_cache = LocalCache(max_size=5, expires=2)
--- setup.py
+++ setup.py
@@ -1,17 +1,43 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os
 from setuptools import setup, find_packages
+
+
+def read_file(*path):
+    base_dir = os.path.dirname(__file__)
+    file_path = (base_dir, ) + tuple(path)
+    return open(os.path.join(*file_path)).read()
+
 
 setup(
     name="PyLocache",
+    url="https://github.com/psjay/PyLocache",
     version="0.0.1",
-    url='http://zhihu.com',
     license='WTFPL',
-    description="Zhihu Domain.",
+    description="PyLocache is a Python implementation of LRU local cache.",
+    long_description=(
+        read_file("README.rst") + "\n\n" +
+        "Change History\n" +
+        "==============\n\n" +
+        read_file("CHANGES.rst")),
     author='psjay',
     author_email="psjay.peng@gmail.com",
+    include_package_data=True,
+    zip_safe=False,
     packages=find_packages(),
     install_requires=[],
     tests_require=['Nose'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2"
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: WTFPL",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ],
 )
